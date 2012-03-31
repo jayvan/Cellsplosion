d1386fe295c4b9efea44dc947682ba4266744dc2
Justin Vanderheide <justin@justinvanderheide.com>
2012-03-30 20:44:15 -0400

    Remove .speed

--- js/game.js
+++ js/game.js
@@ -9,6 +9,5 @@ window.onload = function() {
   var player = Crafty.e("2D, Color, DOM, Multiway")
     .attr({x: 50, y: 50, w: 50, h: 50})
     .color("red")
-    .speed(PLAYER_SPEED)
-    .multiway(3, {W: -90, S: 90, D: 0, A: 180});
+    .multiway(PLAYER_SPEED, {W: -90, S: 90, D: 0, A: 180});
 };
