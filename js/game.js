95804bf944fe4be92bdd38e9bd822457481e1c3d
Justin Vanderheide <justin@justinvanderheide.com>
2012-03-30 20:41:02 -0400

    Get the player moving

--- js/game.js
+++ js/game.js
@@ -1,13 +1,14 @@
 window.onload = function() {
-  var WIDTH     = 800,
-      HEIGHT    = 600;
+  var WIDTH           = 800,
+      HEIGHT          = 600,
+      PLAYER_SPEED    = 3;
 
   Crafty.init(WIDTH, HEIGHT);
   Crafty.background("grey");
 
-  Crafty.e("2D, Color, DOM, Multiway")
+  var player = Crafty.e("2D, Color, DOM, Multiway")
     .attr({x: 50, y: 50, w: 50, h: 50})
     .color("red")
-    .fourway(5)
-    .multiway({W: -90, S: 90, D: 0, A: 180});
+    .speed(PLAYER_SPEED)
+    .multiway(3, {W: -90, S: 90, D: 0, A: 180});
 };
